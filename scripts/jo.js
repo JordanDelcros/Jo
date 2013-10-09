05b8c1f86b6aba2b82f09d0af9948469d4644576
JordanDelcros <contact@jordan-delcros.com>
2013-10-09 12:07:03 +0200

    parseSelector: get id and class

--- scripts/jo.js
+++ scripts/jo.js
@@ -24,17 +24,38 @@
 		},
 		parseSelector: function( selector ){
 
-			selector = selector.replace(/\s*([<>:])\s*/ig, "$1");
+			var returned = new Array();
 
-			selector = selector.split(/\s*,\s*/ig);
+			// REMOVE UNEXPECTED SPACE ON SPECIAL SYMBOLS
+			selector = selector.replace(/\s*([<>:\[\]])\s*/ig, "$1").split(/\s*,\s*/ig);
 
+			// SPLIT SPACE FOREACH SELECTOR
+			for( var key in selector ) selector[key] = selector[key].split(/\s+/ig);
+
+			// TRANSFORM SELECTOR STRING DETAILED TO OBJECT
 			for( var key in selector ){
 
-				var target = selector[key].split(/\s+/ig);
+				for( subkey in selector[key] ){
 
-				console.log(target);
+					var object = new Object();
+
+					object.string = selector[key][subkey];
+
+					// IF CONTAIN ID
+					if( object.string.match(/#/) ) object.id = /#([\w]+)/ig.exec( object.string )[1];
+					// IF CONTAIN CLASS
+					if( object.string.match(/\./) ) object.class = /\.([\w]+)/ig.exec( object.string )[1];
+					// IF CONTAIN 
+
+					selector[key][subkey] = object;
+
+				};
 
 			};
+
+			console.log( selector );
+
+			return returned;
 
 		},
 		each: function( data ){
@@ -52,4 +73,4 @@
 
 })( window );
 
-// header h1, header h1 span, section, p 
+// header#first75_2:hello.class h1.title > span, header>h1 > span , section p
